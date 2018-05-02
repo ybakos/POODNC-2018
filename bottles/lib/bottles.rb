8ed73dbe2c6d2f27140636f2be86136f2c8e684f
Yong Bakos <ybakos@humanoriented.com>
2018-05-02 18:49:06 -0400

    Day 1, Session 3: Extract number-related methods into BottleNumber.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,56 @@
+class BottleNumber
+
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    "#{quantity} #{container}"
+  end
+
+  def quantity
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def container
+    if number == 1
+      "bottle"
+     else
+      "bottles"
+     end
+  end
+
+  def action
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def pronoun
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+end
+
 class Bottles
   def song
     verses(99, 0)
@@ -8,49 +61,13 @@ class Bottles
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
+
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{next_bottle_number} of beer on the wall.\n"
   end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
-  end
-
-  def container(number)
-    if number == 1
-      "bottle"
-     else
-      "bottles"
-     end
-  end
-
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
-  end
-
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
-  end
-
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
-  end
 end
